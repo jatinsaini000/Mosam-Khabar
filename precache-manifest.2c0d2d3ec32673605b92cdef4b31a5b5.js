9cb262e5474451da44f5a2a5f02d886e7b7e7a77
jatinsaini000 <sainijatin505@gmail.com>
2025-05-10 10:42:34 +0530

    Updates

---
+++ precache-manifest.2c0d2d3ec32673605b92cdef4b31a5b5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcc9f11c6a7f68ee44c9a06d8432116",
+    "url": "/Mosam-Khabar/index.html"
+  },
+  {
+    "revision": "b2d655184107948bbf52",
+    "url": "/Mosam-Khabar/static/css/main.445ff79c.chunk.css"
+  },
+  {
+    "revision": "cf05bbf17440a15b33ba",
+    "url": "/Mosam-Khabar/static/js/2.c9622190.chunk.js"
+  },
+  {
+    "revision": "b2d655184107948bbf52",
+    "url": "/Mosam-Khabar/static/js/main.cabcdf40.chunk.js"
+  },
+  {
+    "revision": "52957b1067618dc09f63",
+    "url": "/Mosam-Khabar/static/js/runtime~main.9678af5d.js"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/Mosam-Khabar/static/media/01d.3077681e.svg"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/Mosam-Khabar/static/media/01n.3077681e.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/Mosam-Khabar/static/media/02d.144d9a47.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/Mosam-Khabar/static/media/02n.144d9a47.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/Mosam-Khabar/static/media/03d.8d2c0b2d.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/Mosam-Khabar/static/media/03n.8d2c0b2d.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/Mosam-Khabar/static/media/04d.5a81a676.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/Mosam-Khabar/static/media/04n.5a81a676.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/Mosam-Khabar/static/media/09d.a92066b1.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/Mosam-Khabar/static/media/09n.a92066b1.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/Mosam-Khabar/static/media/10d.810f7538.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/Mosam-Khabar/static/media/10n.810f7538.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/Mosam-Khabar/static/media/11d.0fc0affe.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/Mosam-Khabar/static/media/11n.0fc0affe.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/Mosam-Khabar/static/media/13d.dc58368f.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/Mosam-Khabar/static/media/13n.dc58368f.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/Mosam-Khabar/static/media/50d.1186186b.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/Mosam-Khabar/static/media/50n.1186186b.svg"
+  },
+  {
+    "revision": "bb51f7930bcd0c21b5e6de58ae2005a6",
+    "url": "/Mosam-Khabar/static/media/background.bb51f793.jpg"
+  },
+  {
+    "revision": "961673a4bf41f578a34502a91718adf0",
+    "url": "/Mosam-Khabar/static/media/cloudy_moon.961673a4.svg"
+  }
+]);
